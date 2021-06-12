e723574205c23e896a1d0c1d993bb849b003b02d
Waihon Yew <yewwaihon@gmail.com>
2021-06-12 10:47:15 +0800

    Create basic layout

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -21,8 +21,14 @@ struct ContentView: View {
   @ObservedObject var expenses = Expenses()
   
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      List {
+        ForEach(expenses.items, id: \.name) { item in
+          Text(item.name)
+        }
+      }
+      .navigationBarTitle("iExpense")
+    }
   }
 }
 
