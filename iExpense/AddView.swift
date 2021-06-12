54b75237dc32d1608d92343ae1b535e1e8b7bd3d
Waihon Yew <yewwaihon@gmail.com>
2021-06-12 21:51:35 +0800

    Move the location of AddView.swift to that of ConventView.sift

---
+++ iExpense/AddView.swift
@@ -0,0 +1,49 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Waihon Yew on 12/06/2021.
+//
+
+import SwiftUI
+
+struct AddView: View {
+  @State private var name = ""
+  @State private var type = "Personal"
+  @State private var amount = ""
+  
+  static let types = ["Business", "Personal"]
+  
+  @ObservedObject var expenses: Expenses
+  
+  @Environment(\.presentationMode) var presentationMode
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        TextField("Name", text: $name)
+        Picker("Type", selection: $type) {
+          ForEach(Self.types, id: \.self) {
+            Text($0)
+          }
+        }
+        TextField("Amount", text: $amount)
+          .keyboardType(.numberPad)
+      }
+      .navigationBarTitle("Add new expense")
+      .navigationBarItems(trailing: Button("Save") {
+        if let actualAmount = Int(self.amount) {
+          let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+          self.expenses.items.append(item)
+          self.presentationMode.wrappedValue.dismiss()
+        }
+      })
+    }
+  }
+}
+
+struct AddView_Previews: PreviewProvider {
+  static var previews: some View {
+    AddView(expenses: Expenses())
+  }
+}
