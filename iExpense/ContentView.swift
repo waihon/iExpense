3a2cdde791c6f429b2ced5cf745b23fed01bb597
Waihon Yew <yewwaihon@gmail.com>
2021-06-12 10:52:54 +0800

    Add the ability to add new items for testing purposes

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -28,6 +28,14 @@ struct ContentView: View {
         }
       }
       .navigationBarTitle("iExpense")
+      .navigationBarItems(trailing:
+        Button(action: {
+          let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+          self.expenses.items.append(expense)
+        }) {
+          Image(systemName: "plus")
+        }
+      )
     }
   }
 }
