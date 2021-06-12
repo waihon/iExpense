9ecadfb667b3364a0cd0b03b512d56cc950a6789
Waihon Yew <yewwaihon@gmail.com>
2021-06-12 12:48:07 +0800

    Crate a basic layout for AddView

--- AddView.swift
+++ AddView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct AddView: View {
+  @State private var name = ""
+  @State private var type = "Personal"
+  @State private var amount = ""
+  
+  static let types = ["Business", "Personal"]
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    NavigationView {
+      Form {
+        TextField("Name", text: $name)
+        Picker("Type", selection: $type) {
+          ForEach(Self.types, id: \.self) {
+            Text($0)
+          }
+        }
+        TextField("Amount", text: $amount)
+          .keyboardType(.numberPad)
+      }
+      .navigationBarTitle("Add new expense")
+    }
   }
 }
 
