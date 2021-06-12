f4523f9eb2d3652d0a5861576ebe2cbd669b31db
Waihon Yew <yewwaihon@gmail.com>
2021-06-12 12:26:43 +0800

    Add a new view for adding new expense items

---
+++ AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Waihon Yew on 12/06/2021.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
